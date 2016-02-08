0ae2deff5a08029cecd0e089946bfc710fa14d9c
Artem <ladrower@gmail.com>
2016-02-08 16:47:32 +0100

    two triangles

--- OpenGL1/main.cpp
+++ OpenGL1/main.cpp
@@ -106,20 +106,26 @@ int main()
 
 	// Rectangle vertices
 	GLfloat vertices[] = {
+		// First triangle
+		0.9f, -0.5f, 0.0f,
+		0.5f,  0.5f, 0.0f,
+		0.1f, -0.5f, 0.0f,
+		// Second triangle
+		-0.9f, -0.5f, 0.0f,
+		-0.5f,   0.5f, 0.0f,
+		-0.1f,  -0.5f, 0.0f,
+
+		/*
 		0.5f, 0.5f, 0.0f,  // Top Right
 		0.5f, -0.5f, 0.0f,  // Bottom Right
 		-0.5f, -0.5f, 0.0f,  // Bottom Left
 		-0.5f, 0.5f, 0.0f   // Top Left 
-	};
-	GLuint indices[] = {  // Note that we start from 0!
-		0, 1, 3,   // First Triangle
-		1, 2, 3    // Second Triangle
+		*/
 	};
 
-	GLuint VBO, VAO, EBO;
+	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
 	glBindVertexArray(VAO);
@@ -127,8 +133,6 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
@@ -154,9 +158,8 @@ int main()
 		// Draw our first triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
 
 		// Swap double buffers
 		glfwSwapBuffers(window);
@@ -165,7 +168,6 @@ int main()
 	// Properly de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
 
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();

9a4c48ce5830dc604ad5958b8861eee2637dd242
Artem <ladrower@gmail.com>
2016-02-10 15:14:32 +0100

    2 triangles using two different VAOs and VBOs for their data

--- OpenGL1/main.cpp
+++ OpenGL1/main.cpp
@@ -103,36 +103,47 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-
-	// Rectangle vertices
-	GLfloat vertices[] = {
+	GLfloat firstVertices[] = {
 		// First triangle
 		0.9f, -0.5f, 0.0f,
-		0.5f,  0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 		0.1f, -0.5f, 0.0f,
-		// Second triangle
-		-0.9f, -0.5f, 0.0f,
-		-0.5f,   0.5f, 0.0f,
-		-0.1f,  -0.5f, 0.0f,
-
-		/*
-		0.5f, 0.5f, 0.0f,  // Top Right
-		0.5f, -0.5f, 0.0f,  // Bottom Right
-		-0.5f, -0.5f, 0.0f,  // Bottom Left
-		-0.5f, 0.5f, 0.0f   // Top Left 
-		*/
 	};
 
-	GLuint VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	GLfloat secondVertices[] = {
+		// Second triangle
+		-0.9f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		-0.1f, -0.5f, 0.0f,
+	};
 
+	GLuint VBOS[2], VAOS[2];
+	
+
+	glGenVertexArrays(2, VAOS);
+	glGenBuffers(2, VBOS);
+
+
+	//--- Bind the first triangle 
 	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAOS[0]);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOS[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(firstVertices), firstVertices, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
+
+	//--- Bind the second triangle 
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAOS[1]);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBOS[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondVertices), secondVertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
@@ -142,7 +153,7 @@ int main()
 	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
 
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_COLOR);
 
 	// Run game loop
 	while (!glfwWindowShouldClose(window))
@@ -155,10 +166,17 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw our first triangle
+		// Use the same shader for both triangles
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		// Draw the first triangle
+		glBindVertexArray(VAOS[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Draw the second triangle
+		glBindVertexArray(VAOS[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glBindVertexArray(0);
 
 		// Swap double buffers
@@ -166,8 +184,8 @@ int main()
 	}
 
 	// Properly de-allocate all resources once they've outlived their purpose
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(2, VAOS);
+	glDeleteBuffers(2, VBOS);
 
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
